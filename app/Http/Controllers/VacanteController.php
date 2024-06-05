ca8c6029d23e85f4d27257c3cc2b44d7bc3708dd
Diego Alcaraz Zúñiga <diego.alcaraz@avanzasolido.com>
2024-06-05 10:06:40 -0600

    vistas

--- app/Http/Controllers/VacanteController.php
+++ app/Http/Controllers/VacanteController.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 class VacanteController extends Controller
 {
     public function index(){
-        return view('vacante.index');
+        return view("vacante.index");
     }
     public function create(){
     }
