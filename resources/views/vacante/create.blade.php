51ba385959217932fac16596b03643660c97ab67
Diego Alcaraz Zúñiga <diego.alcaraz@avanzasolido.com>
2024-06-07 15:18:59 -0600

    intento de diseño

--- resources/views/vacante/create.blade.php
+++ resources/views/vacante/create.blade.php
@@ -7,24 +7,3 @@
     <h1>puedes crear un vacante</h1>
     
 @endsection
-<style>
-    
-</style>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    <div class="logo">
-        <nav>
-            <img src="../imagenes/logo.jpg" alt="foto" width="50" height="50">
-            <p style="top: 100px; left:20px;">Bolsa de trabajo</p><br><p>tu mejor opción para encontrar un puesto de trabajo</p>
-        </nav>
-    </div>
-    
-</body>
-</html>
