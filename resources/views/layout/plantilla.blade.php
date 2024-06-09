fdfa86c195f7e864579582aec643877521e528a6
diegoalcaraz98 <diego.alcaraz@avanzasolido.com>
2024-06-09 08:41:13 -0600

    plantillas kawaii

--- resources/views/layout/plantilla.blade.php
+++ resources/views/layout/plantilla.blade.php
@@ -5,8 +5,30 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title')</title>
 </head>
-<body>
-
+<style>
+        .arriba{
+            background-color:aquamarine; 
+        }
+        .logo{
+            width: 50px;
+            height: 50px;
+        }
+        .bol{
+            position: absolute;
+            top: -4px;
+            left: 62px;
+        }
+        .trab{
+            position: absolute;
+            top: 11px;
+            left: 62px;
+        }
+</style>
+<body class="fon">
+    <nav class="arriba">
+        <img src="./imagenes/logo.jpg" alt="foto" class="logo">
+        <h3 class="bol">Bolsa de trabajo</h3><br><h4 class="trab">tu mejor opcion para encontrar trabajo</h>
+    </nav>
     @yield('content')
 </body>
 </html>
--- resources/views/vacante/show.blade.php
+++ resources/views/vacante/show.blade.php
@@ -1,8 +1,59 @@
-@extends('layout.plantilla')
+<!DOCTYPE html>
+<html lang="en">
 
-@section('title', 'show')
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<style>
+    .arriba {
+        background-color: aquamarine;
+    }
 
-@section('content')
-    <h1>bienvenido a la vacante <?php echo $vacante; ?></h1>
-    <img src="../imagenes/logo.jpg" alt="foto">
-@endsection
+    .logo {
+        width: 50px;
+        height: 50px;
+    }
+
+    .bol {
+        position: absolute;
+        top: -4px;
+        left: 62px;
+    }
+
+    .trab {
+        position: absolute;
+        top: 11px;
+        left: 62px;
+    }
+    table{
+        border-block-color: red;
+        border-collapse: separate;
+    }
+</style>
+
+<body class="fon">
+    <nav class="arriba">
+        <img src="../imagenes/logo.jpg" alt="foto" class="logo">
+        <h3 class="bol">Bolsa de trabajo</h3><br>
+        <h4 class="trab">tu mejor opcion para encontrar trabajo</h>
+    </nav>
+    <div>
+        <table>
+            <tr>avanza solido</tr>
+        </table>
+            <tr>administrador de base de datos</tr>
+        </table>
+            <tr>cuidas y mantienes la base de datos</tr>
+        </table>
+        <table>
+            <tr>ambar rojo</tr>
+        </table>
+            <tr>administrador de base de datos</tr>
+        </table>
+            <tr>cuidas y mantienes la base de datos</tr>
+        </table>
+    </div>
+</body>
+
+</html>
